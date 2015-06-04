b3977404b5f5a1f2bf02bb40b1423ab52782a259
Abhijit Deb <abhijitdeb@gmail.com>
2015-06-04 08:58:30 -0400

    Added comments

--- plot1.R
+++ plot1.R
@@ -1,15 +1,19 @@
+## Get the data from the file 
 getData <- function() {
         plotdata <- read.csv("data/household_power_consumption-2.txt", 
                              sep = ";", 
                              header = TRUE,
                              as.is = TRUE,
                              na.strings = c("?"))
+        ## Get the data for only some specific dates
         plotdata <- subset(plotdata, Date == "1/2/2007" | Date == "2/2/2007")
+        ## Add a datetime column so that we can sort data by date
         plotdata[, "DateTime"] <- paste(plotdata$Date, plotdata$Time)
         plotdata$DateTime <- strptime(plotdata$DateTime, "%d/%m/%Y %H:%M:%S")
         plotdata        
 }
 
+## Plot the data
 plot1 <- function(plotdata) {       
         #Plot 1        
         startPng("plot1.png")
@@ -17,6 +21,7 @@ plot1 <- function(plotdata) {
         dev.off()
 }
 
+## Save the image to a file
 startPng <- function(filename) {
         png(file = filename, bg="transparent", width = 480, height = 480)
 }
--- plot3.R
+++ plot3.R
@@ -1,16 +1,19 @@
+## Get the data from the file 
 getData <- function() {
         plotdata <- read.csv("data/household_power_consumption-2.txt", 
                              sep = ";", 
                              header = TRUE,
                              as.is = TRUE,
                              na.strings = c("?"))
+        ## Get the data for only some specific dates
         plotdata <- subset(plotdata, Date == "1/2/2007" | Date == "2/2/2007")
+        ## Add a datetime column so that we can sort data by date
         plotdata[, "DateTime"] <- paste(plotdata$Date, plotdata$Time)
         plotdata$DateTime <- strptime(plotdata$DateTime, "%d/%m/%Y %H:%M:%S")
         plotdata        
 }
 
-
+## Plot the data
 plot3 <- function(plotdata) {  
         startPng("plot3.png")
         #Plot 3
@@ -27,6 +30,7 @@ plot3 <- function(plotdata) {
         dev.off()
 }
 
+## Save the image to a file
 startPng <- function(filename) {
         png(file = filename, bg="transparent", width = 480, height = 480)
 }
